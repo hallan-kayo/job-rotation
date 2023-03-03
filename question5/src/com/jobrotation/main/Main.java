93319cddf6eab71763a9b9a52e93e4d8c28eaad1
Halan Caio <hallankayo20@gmail.com>
2023-03-03 09:55:33 -0300

    question 5

---
+++ question5/src/com/jobrotation/main/Main.java
@@ -0,0 +1,14 @@
+package com.jobrotation.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string = "halan caio";
+		System.out.println(string);
+		StringBuilder strb = new StringBuilder(string);
+		string = strb.reverse().toString();
+		System.out.println(string);
+	}
+
+}
